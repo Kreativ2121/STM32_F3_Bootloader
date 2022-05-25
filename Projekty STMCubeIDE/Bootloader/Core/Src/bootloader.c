d00f142ba1977832e147d53a5d49d0cec98f42b5
Dominik Ciesiołkiewicz <cd44289@zut.edu.pl>
2022-05-25 15:36:58 +0200

    Dodano funkcje dotyczące operowania pamięcią FLASH w programie Bootloadera.

--- Projekty STMCubeIDE/Bootloader/Core/Src/bootloader.c
+++ Projekty STMCubeIDE/Bootloader/Core/Src/bootloader.c
@@ -2,6 +2,63 @@
 
 typedef void (*pFunction)(void);
 
+//Zapisywanie aplikacji użytkownika do FLASH
+uint8_t WriteUserApplication(uint32_t* data, uint32_t dataSize, uint32_t offset) {
+    if (HAL_FLASH_Unlock() == HAL_OK) {
+        for (int i = 0; i < dataSize; i++) {
+            HAL_StatusTypeDef success = HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, APP_ADDRESS + offset + (i * 4), data[i]);
+
+            if (success != HAL_OK) {
+                HAL_FLASH_Lock();
+                return 0;
+            }
+        }
+        HAL_FLASH_Lock();
+    } else {
+        return 0;
+    }
+
+    return 1;
+}
+
+//Usuwanie aplikacji użytkownika z pamięci FLASH
+uint8_t EraseUserApplication() {
+    HAL_StatusTypeDef success = HAL_ERROR;
+    uint32_t errorSector = 0;
+
+    if (HAL_FLASH_Unlock() == HAL_OK) {
+        FLASH_EraseInitTypeDef eraseInit = {0};
+        eraseInit.NbPages = 96; // Count of pages to erase
+        eraseInit.PageAddress = 0x08010000; // Start address of the first page to erase
+        eraseInit.TypeErase = FLASH_TYPEERASE_PAGES;
+
+        success = HAL_FLASHEx_Erase(&eraseInit, &errorSector);
+
+        HAL_FLASH_Lock();
+    }
+
+    return success == HAL_OK ? 1 : 0;
+}
+
+//Funkcja wysyłająca dane do odczytu poprzez COM
+void SendBackup() {
+    uint8_t* app = (uint8_t *) (APP_ADDRESS);
+
+    // Send 192 kB of user application
+    for (int i = 0; i < 192 * 1024; i++) {
+        //SendByte(app[i]); //TUTAJ WYWOLAC FUNKCJE DO WYSYLANIA JEDNEGO BAJTU
+    }
+}
+
+//Funkcja do sprawdzania czy aplikacja użytkownika jest w bootloaderze
+uint8_t UserApplicationExists() {
+    uint32_t bootloaderMspValue = *(uint32_t *) (FLASH_BASE);
+    uint32_t appMspValue = *(uint32_t *) (APP_ADDRESS);
+
+    return appMspValue == bootloaderMspValue ? 1 : 0;
+}
+
+//Funkcja do przeskoczenia z bootloadera do aplikacji użytkownika
 void JumpToAddress(uint32_t addr) {
     uint32_t JumpAddress = *(uint32_t *) (addr + 4); //Definitions for jump
     pFunction Jump = (pFunction) JumpAddress; //Definitions for jump
